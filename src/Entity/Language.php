73b7b4d1c1189ac7dcba76c3f51ec11ec3a80610
Prohorovych Andrew <prohorovychua@gmail.com>
2017-12-22 20:16:47 +0200

    add language entity

---
+++ src/Entity/Language.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\LanguageRepository")
+ * @ORM\Table(name="languages")
+ */
+class Language
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     * @Assert\NotBlank()
+     */
+    private $name;
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId(): ? int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Language
+     */
+    public function setName($name): ? Language
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
