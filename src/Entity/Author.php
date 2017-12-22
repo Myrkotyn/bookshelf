64ed36f3d7e054f26ca6532b6faa89ec39b9ed4f
Prohorovych Andrew <prohorovychua@gmail.com>
2017-12-22 20:20:45 +0200

    add author entity

---
+++ src/Entity/Author.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\AuthorRepository")
+ * @ORM\Table(name="authors")
+ */
+class Author
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     * @Assert\NotBlank()
+     */
+    private $name;
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId():? int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set name
+     *
+     * @param mixed $name
+     * @return Author
+     */
+    public function setName($name): ?Author
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
